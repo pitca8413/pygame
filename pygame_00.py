a518a9dc07081a71263704c4f3636dfdb7f67608
pitca8413 <pitca8413@gmail.com>
2020-04-11 14:14:26 +0900

    pygame 프로젝트 생성

--- pygame_00.py
+++ pygame_00.py
@@ -7,5 +7,9 @@
     2. pygame module 설치하기
         2-1. file / setting / pygame interpreter
     3. git과 연동하기
+        3-1. VCS / import into version control / share project on github
+        3-2. venv 가상환경 연동 해제 하기
+    4. pygame_00.py 파일 commit
+    5. pygame_00.py 파일 git 저장소에 push
 
 '''
